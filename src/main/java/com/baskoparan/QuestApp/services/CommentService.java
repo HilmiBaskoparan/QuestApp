89093c41f248a228a17384b97d4c41067c285d66
hilmibaskoparan <h.baskoparan@gmail.com>
2024-02-20 17:05:01 +0300

    Revert "Revert "Master to Frontend""
    This reverts commit 02480d47dbbd47774eebf57a317d0b12f7f2bcbc.

---
+++ src/main/java/com/baskoparan/QuestApp/services/CommentService.java
@@ -0,0 +1,21 @@
+package com.baskoparan.QuestApp.services;
+
+import com.baskoparan.QuestApp.business.requests.CommentCreateRequest;
+import com.baskoparan.QuestApp.business.requests.CommentUpdateRequest;
+import com.baskoparan.QuestApp.entities.Comment;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CommentService {
+
+    List<Comment> getAllCommentsWithParam(Optional<Long> userId, Optional<Long> postId);
+
+    Comment createComment(CommentCreateRequest commentCreateRequest);
+
+    Comment updateComment(Long commentId, CommentUpdateRequest commentUpdateRequest);
+
+    void deleteComment(Long commentId);
+
+    Comment findById(Long commentId);
+}
